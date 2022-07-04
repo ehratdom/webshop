49420aecab8b2a3b01f718c1934ca653b3bf2841
“Dominic <ehratdom@students.zhaw.ch>
2022-07-04 12:02:12 +0200

    Test

--- frontend/src/views/Projects.vue
+++ frontend/src/views/Projects.vue
@@ -8,7 +8,7 @@
     <ion-content :fullscreen="true">
       <ion-header collapse="condense">
         <ion-toolbar>
-          <ion-title size="large">Projects</ion-title>
+          <ion-title size="large">Pojects</ion-title>
         </ion-toolbar>
       </ion-header>
     </ion-content>
