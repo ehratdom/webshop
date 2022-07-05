226008967f7f647c30608823d91a965837313a97
xecret <luca.baserga@gmail.com>
2022-07-05 09:26:52 +0200

    added Endpoint files

---
+++ backend/src/main/java/ch/zhaw/sml/iwi/meng/leantodo/boundary/ShoppingCartEndpoint.java
@@ -0,0 +1,19 @@
+package ch.zhaw.sml.iwi.meng.leantodo.boundary;
+
+import java.security.Principal;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import ch.zhaw.sml.iwi.meng.leantodo.controller.ToDoController;
+import ch.zhaw.sml.iwi.meng.leantodo.entity.ToDo;
+
+@RestController
+public class ShoppingCartEndpoint {
+    
+}
