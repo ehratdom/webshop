cece36f009266a333309091c056a1dd1ba070725
Loren Hyseni <hysenlor@students.zhaw.ch>
2022-07-06 15:21:16 +0200

    pusher

--- frontend/src/views/Cart.vue
+++ frontend/src/views/Cart.vue
@@ -11,15 +11,56 @@
           <ion-title size="large">Cart</ion-title>
         </ion-toolbar>
       </ion-header>
+      <ion-list>
+        <ion-item :key="user" v-for="user in users">
+          <ion-grid>
+            <ion-row>
+              <ion-col>
+                {{ user.shoppingCart }}
+              </ion-col>
+              <ion-col> </ion-col>
+            </ion-row>
+          </ion-grid>
+        </ion-item>
+      </ion-list>
     </ion-content>
   </ion-page>
 </template>
 
 <script lang="ts">
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
+import {
+  IonPage,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonCol,
+  IonRow,
+  IonGrid,
+  IonItem,
+  IonList,
+} from "@ionic/vue";
+import { defineComponent } from "vue";
+import { useUsers } from "@/composables/useUsers";
 
-export default  {
-  name: 'Cart',
-  components: { IonHeader, IonToolbar, IonTitle, IonContent, IonPage }
-}
+export default defineComponent({
+  name: "Todo",
+  components: {
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonPage,
+    IonCol,
+    IonRow,
+    IonGrid,
+    IonItem,
+    IonList,
+  },
+  setup() {
+    const { users } = useUsers();
+
+    return { users };
+  },
+});
 </script>
