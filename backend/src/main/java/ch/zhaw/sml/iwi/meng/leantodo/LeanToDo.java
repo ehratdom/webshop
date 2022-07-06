ec92bbc301a8c436deb94d70eacf67afd8bd9c36
xecret <luca.baserga@gmail.com>
2022-07-06 10:44:40 +0200

    fixed immage errors

--- backend/src/main/java/ch/zhaw/sml/iwi/meng/leantodo/LeanToDo.java
+++ backend/src/main/java/ch/zhaw/sml/iwi/meng/leantodo/LeanToDo.java
@@ -93,21 +93,63 @@ public class LeanToDo implements CommandLineRunner {
         product.setName("T-Shirt");
         product.setDescription("langwiligs tshirt");
         product.setPrice(25);
-        product.setImg("00003aeb-ace5-43bf-9a0c-dc31a03e9cd2.jpg");
+        product.setImg("http://localhost:8080/images/00003aeb-ace5-43bf-9a0c-dc31a03e9cd2.jpg");
         productRepository.save(product);
 
         product = new Product();
         product.setName("Hoodie");
         product.setDescription("langwilige hoodie");
         product.setPrice(55);
-        product.setImg("000d31f6-44b5-4758-8d86-c4fcfb234392.jpg");
+        product.setImg("http://localhost:8080/images/000d31f6-44b5-4758-8d86-c4fcfb234392.jpg");
         productRepository.save(product);
 
         product = new Product();
         product.setName("Polo");
         product.setDescription("langwiligs Polo-Shirt");
         product.setPrice(35);
-        product.setImg("001068a8-86c3-4029-a9d9-1e27015e7e64.jpg");
+        product.setImg("http://localhost:8080/images/001068a8-86c3-4029-a9d9-1e27015e7e64.jpg");
+        productRepository.save(product);
+
+        product = new Product();
+        product.setName("Shirt");
+        product.setDescription("langwiligs Shirt");
+        product.setPrice(35);
+        product.setImg("http://localhost:8080/images/00143901-a14c-4600-960f-7747b4a3a8cd.jpg");
+        productRepository.save(product);
+
+        product = new Product();
+        product.setName("Outwear");
+        product.setDescription("Outwear1");
+        product.setPrice(40);
+        product.setImg("http://localhost:8080/images/00149032-3dd6-426e-9bc0-d53032536a42.jpg");
+        productRepository.save(product);
+
+        product = new Product();
+        product.setName("Outwear");
+        product.setDescription("Outwear2");
+        product.setPrice(40);
+        product.setImg("http://localhost:8080/images/00208c77-ee58-4b63-bc1a-b9b3aa4f1be0.jpg");
+        productRepository.save(product);
+
+        product = new Product();
+        product.setName("Outwear");
+        product.setDescription("Outwear3");
+        product.setPrice(40);
+        product.setImg("http://localhost:8080/images/0028956e-9962-46f0-ad45-2c42f8a59d94.jpg");
+        productRepository.save(product);
+
+        product = new Product();
+        product.setName("Undershirt");
+        product.setDescription("dreckigs Undershirt");
+        product.setPrice(30);
+        product.setImg("http://localhost:8080/images/002e88c2-e0ce-4a4b-9670-eddc3d99accc.jpg");
+        productRepository.save(product);
+
+        product = new Product();
+        product.setName("Pants");
+        product.setDescription("langwiligi Hose");
+        product.setPrice(60);
+        product.setImg("http://localhost:8080/images/002eb5b8-6541-42a3-9596-0d94f7b866ae.jpg");
         productRepository.save(product);
 
 
