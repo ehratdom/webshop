faedc058767e430bd6fd2e316ac36f9013af8e4c
Loren Hyseni <hysenlor@students.zhaw.ch>
2022-07-07 17:28:24 +0200

    frontend test

--- frontend/src/composables/useUsers.ts
+++ frontend/src/composables/useUsers.ts
@@ -1,6 +1,25 @@
-import { getSCfromUser } from '@/api/users';
+import { getSCfromUser, getSCfromUsers } from '@/api/users';
 import { User } from '@/model/user';
 import { onMounted, ref } from 'vue';
+
+export function useUser() {
+
+    const user = ref<User[]>([]);
+
+    const getUser = async () => {
+        try {
+            user.value = await getSCfromUser();
+        } catch (error) {
+            console.log(error); // FIXME: Errorhandling
+        }
+    }
+
+    onMounted(getUser);
+
+    return {
+        user
+    }
+}
 
 export function useUsers() {
 
@@ -8,7 +27,7 @@ export function useUsers() {
 
     const getUsers = async () => {
         try {
-            users.value = await getSCfromUser();
+            users.value = await getSCfromUsers();
         } catch (error) {
             console.log(error); // FIXME: Errorhandling
         }
