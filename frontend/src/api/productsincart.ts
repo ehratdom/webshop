be74b1326c50eece6353cfb254932f394a2a2a9c
Loren Hyseni <hysenlor@students.zhaw.ch>
2022-07-08 13:28:07 +0200

    upt frontend

--- frontend/src/api/productsincart.ts
+++ frontend/src/api/productsincart.ts
@@ -3,11 +3,13 @@ import { API_ROOT } from "@/config/development";
 import { ProductinCart } from '@/model/productincart';
 
 export async function deleteProductInCart(): Promise<ProductinCart[]> {
+
+
     const config = {        
         withCredentials: true
     }
     try {
-        const response = await axios.delete(API_ROOT + '/api/deleteitem/12', config);
+        const response = await axios.delete(API_ROOT + '/api/deleteitem/', config);
         return response.data;
     } catch (error) {
         return <any>error;   
